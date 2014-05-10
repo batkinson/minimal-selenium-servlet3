62b8286e41442e7f6318adc70ea4c536241f214b
Brent Atkinson <brent.atkinson@gmail.com>
2014-05-10 09:59:17 -0400

    Added success status to screenshot file names.

--- src/test/java/com/github/batkinson/SampleIT.java
+++ src/test/java/com/github/batkinson/SampleIT.java
@@ -10,6 +10,8 @@ import org.testng.annotations.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.testng.Assert.assertTrue;
 
@@ -40,7 +42,8 @@ public class SampleIT {
 
     @AfterMethod
     public void takeScreenshot(ITestResult result) throws IOException {
-        String shotName = String.format("%s-%s.png", result.getTestClass().getName(), result.getName());
+        String successStatus = result.isSuccess() ? "SUCCESS" : "NOSUCCESS";
+        String shotName = String.format("%s-%s-%s.png", successStatus, result.getTestClass().getName(), result.getName());
         File finalShotFile = new File(screenshotDir, shotName);
         File shotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(shotFile, finalShotFile);
