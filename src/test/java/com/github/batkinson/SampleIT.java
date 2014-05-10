e1e88e59c7beb129046feb972c6baa13cc616b7a
Brent Atkinson <brent.atkinson@gmail.com>
2014-05-10 09:43:53 -0400

    Added web test screenshots.

--- src/test/java/com/github/batkinson/SampleIT.java
+++ src/test/java/com/github/batkinson/SampleIT.java
@@ -1,17 +1,31 @@
 package com.github.batkinson;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+
+import java.io.File;
+import java.io.IOException;
 
 import static org.testng.Assert.assertTrue;
 
 public class SampleIT {
 
+    private String appUrl;
+    private File screenshotDir;
+
     WebDriver driver;
+
+    @Parameters({"appUrl", "screenshotDir"})
+    public SampleIT(String appUrl, String screenshotDir) {
+        this.appUrl = appUrl;
+        this.screenshotDir = new File(screenshotDir);
+        this.screenshotDir.mkdirs();
+    }
 
     @BeforeClass
     public void createDriver() {
@@ -19,14 +33,22 @@ public class SampleIT {
     }
 
     @Test
-    @Parameters("appUrl")
-    public void testHello(String appUrl) {
+    public void testHello() {
         driver.get(appUrl);
         assertTrue(driver.getPageSource().contains("Hello!"), "Body should contain text 'Hello'");
+    }
+
+    @AfterMethod
+    public void takeScreenshot(ITestResult result) throws IOException {
+        String shotName = String.format("%s-%s.png", result.getTestClass().getName(), result.getName());
+        File finalShotFile = new File(screenshotDir, shotName);
+        File shotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(shotFile, finalShotFile);
     }
 
     @AfterClass
     public void quitDriver() {
         driver.quit();
     }
+
 }
